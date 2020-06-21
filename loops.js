bbf8ebfaaad9631e3c04082db3c4812ecb55fe15
MuathAlawi Alawi <muathalawi1@gmail.com>
2020-06-21 05:00:01 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -19,10 +19,15 @@ function whileLoop(n) {
   return "done";
 }
 
-function doWhileLoop(num) {
-  function incrementVariable() {
-    console.log('I run once regardless')
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
   }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
 }
   
   
